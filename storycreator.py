edc9b5fbb975071ecf6b17dab83d88f6556e4f31
merlin0509 <71698972+merlin0509@users.noreply.github.com>
2020-09-22 15:10:05 +0530

    Create storycreator.py

---
+++ storycreator.py
@@ -0,0 +1,12 @@
+title=input("Enter the title of the story")
+pronoun=input("Enter the protagonist name")
+sex=input("Is it a he/she?")
+liquid=input("Enter a drink")
+feeling=input("Enter a feeling")
+
+print("Hi, this story is called {0} and its about {1} who liked to swim, one day, \ 
+              {2} decided to swim in{3} and {2} was having a {4} time. \
+              While {2} was swimming everyone around started making fun of the whole scene so {2} felt terribly embarrased \
+              and  felt the right thinng to do is leave. \
+              After reaching home the mother asked about the day and {2} started crying."\
+              .format(title,pronoun,sex,liquid,feeling,sex))
